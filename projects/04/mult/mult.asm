0c5d0188689fd0a2ea03c39c20c955e68e059bab
KaFai Choi <kafai@river.com>
2019-12-22 11:00:57 +0700

    implement mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R1
+  D=M
+@i // counter for R1
+  M=D
+@product
+  M=0
+(LOOP)
+  @i
+  D=M
+  @RESULT
+  D;JEQ
+  @R0
+  D=M
+  @product
+  M=M+D
+  @i
+  M=M-1
+  @LOOP
+  0;JMP
+(RESULT)
+  @product
+  D=M
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
